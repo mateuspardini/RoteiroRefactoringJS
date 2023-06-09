db698e7c64acbe1c4a3c8de1436d012e3ffd4e9d
Mateus Pardini <mateuspardini6@gmail.com>
2023-06-09 12:48:52 -0300

    Commit 6 - Move Function

--- index.js
+++ index.js
@@ -1,74 +1,75 @@
 const { readFileSync } = require("fs");
 
-function gerarFaturaStr(fatura, pecas) {
-  function formatarMoeda(valor) {
-    const formato = new Intl.NumberFormat("pt-BR", {
-      style: "currency",
-      currency: "BRL",
-      minimumFractionDigits: 2,
-    });
+function formatarMoeda(valor) {
+  const formato = new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+    minimumFractionDigits: 2,
+  });
 
-    return formato.format(valor / 100);
+  return formato.format(valor / 100);
+}
+
+function calcularTotalApresentacao(apre, pecas) {
+  let total = 0;
+
+  switch (getPeca(apre, pecas).tipo) {
+    case "tragedia":
+      total = 40000;
+      if (apre.audiencia > 30) {
+        total += 1000 * (apre.audiencia - 30);
+      }
+      break;
+    case "comedia":
+      total = 30000;
+      if (apre.audiencia > 20) {
+        total += 10000 + 500 * (apre.audiencia - 20);
+      }
+      total += 300 * apre.audiencia;
+      break;
+    default:
+      throw new Error(`Peça desconhecida: ${getPeca(apre, pecas).tipo}`);
   }
 
-  function calcularTotalApresentacao(apre) {
-    let total = 0;
+  return total;
+}
 
-    switch (getPeca(apre).tipo) {
-      case "tragedia":
-        total = 40000;
-        if (apre.audiencia > 30) {
-          total += 1000 * (apre.audiencia - 30);
-        }
-        break;
-      case "comedia":
-        total = 30000;
-        if (apre.audiencia > 20) {
-          total += 10000 + 500 * (apre.audiencia - 20);
-        }
-        total += 300 * apre.audiencia;
-        break;
-      default:
-        throw new Error(`Peça desconhecida: ${getPeca(apre).tipo}`);
-    }
+function getPeca(apresentacao, pecas) {
+  return pecas[apresentacao.id];
+}
 
-    return total;
-  }
+function calcularCredito(apre, pecas) {
+  let creditos = 0;
+  creditos += Math.max(apre.audiencia - 30, 0);
+  if (getPeca(apre, pecas).tipo === "comedia")
+    creditos += Math.floor(apre.audiencia / 5);
+  return creditos;
+}
 
-  function getPeca(apresentacao) {
-    return pecas[apresentacao.id];
-  }
-
-  function calcularCredito(apre) {
-    let creditos = 0;
-    creditos += Math.max(apre.audiencia - 30, 0);
-    if (getPeca(apre).tipo === "comedia")
-      creditos += Math.floor(apre.audiencia / 5);
-    return creditos;
-  }
-
-  function calcularTotalFatura() {
-    let totalFatura = 0;
-    for (let apre of fatura.apresentacoes) {
-      totalFatura += calcularTotalApresentacao(apre);
-    }
-    return totalFatura;
-  }
-
-  function calcularTotalCreditos() {
-    let totalCreditos = 0;
-    for (let apre of fatura.apresentacoes) {
-      totalCreditos += calcularCredito(apre);
-    }
-    return totalCreditos;
-  }
-
-  let faturaStr = `Fatura ${fatura.cliente}\n`;
+function calcularTotalFatura(fatura, pecas) {
+  let totalFatura = 0;
   for (let apre of fatura.apresentacoes) {
-    faturaStr += `  ${getPeca(apre).nome}: ${formatarMoeda(calcularTotalApresentacao(apre))} (${apre.audiencia} assentos)\n`;
+    totalFatura += calcularTotalApresentacao(apre, pecas);
   }
-  faturaStr += `Valor total: ${formatarMoeda(calcularTotalFatura())}\n`;
-  faturaStr += `Créditos acumulados: ${calcularTotalCreditos()} \n`;
+  return totalFatura;
+}
+
+function calcularTotalCreditos(fatura, pecas) {
+  let totalCreditos = 0;
+  for (let apre of fatura.apresentacoes) {
+    totalCreditos += calcularCredito(apre, pecas);
+  }
+  return totalCreditos;
+}
+
+function gerarFaturaStr(fatura, pecas) {
+  let faturaStr = `Fatura ${fatura.cliente}\n`;
+
+  for (let apre of fatura.apresentacoes) {
+    faturaStr += `  ${getPeca(apre, pecas).nome}: ${formatarMoeda(calcularTotalApresentacao(apre, pecas))} (${apre.audiencia} assentos)\n`;
+  }
+  faturaStr += `Valor total: ${formatarMoeda(calcularTotalFatura(fatura, pecas))}\n`;
+  faturaStr += `Créditos acumulados: ${calcularTotalCreditos(fatura, pecas)} \n`;
   return faturaStr;
 }
 
